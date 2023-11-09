453d4e97ec376aecb1ab5010ee69aa4359ee5b5b
SRIDHARALAPATI <sridharalapati22@gmail.com>
2023-11-09 17:07:23 -0600

    Eposide -1

---
+++ app.js
@@ -0,0 +1,31 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement(
+        "h1",
+        { id: "heading" },
+        " i have just created nested elements using react"
+      ),
+      React.createElement("h2", { id: "heading" }, " H2 tag is here"),
+    ]),
+
+    React.createElement("div", { id: "child2" }, [
+        React.createElement(
+          "h1",
+          { id: "heading" },
+          " i have just created nested elements using react"
+        ),
+        React.createElement("h2", { id: "heading" }, " H2 tag is here"),
+      ])]
+);
+console.log(parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
