bd4ecdc387c7ede735c96023f2f99916b975f330
SRIDHARALAPATI <sridharalapati22@gmail.com>
2023-12-31 21:07:08 -0600

    Eposide 3

--- Eposide-3/app.js
+++ Eposide-3/app.js
@@ -13,16 +13,21 @@ const TitleComponent=()=>(
     <h1>Learning React from Namaste React </h1>
 </div>
 );
+// we can write javascript code inside jsx using curly braces. ie {javascript code}
+
+const number=100+500;
+
+
 // component compostiion  getting other functinal component code and rendering. example of component composition
 const HeadingComponent=()=>(
    <div id="container">
-    <TitleComponent/>  
+    <h1>{number}</h1>
+     {TitleComponent()} {/*we can call Functional component inside {} */}
+    <TitleComponent/> {/*normal way of rendering */}
+    <TitleComponent></TitleComponent> {/* other way */}
    <h3>You created Functional Component</h3>
    </div>
 );
-
-
-
 
 const image = <img src="http://placekitten.com/g/200/300"></img>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
